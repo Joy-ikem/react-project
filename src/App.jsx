29f4f99f14414f9d2391551cf353c0c314f70370
react-project <ikemjoy57@gmail.com>
2025-04-08 10:35:35 +0100

    Updated import from footer to Last

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,24 @@
 import React from 'react'
 import './App.css'
-import Header from './components/Header';
+import Begin from './components/Begin';
 import Hero from './components/Hero';
 import Home from './components/Home';
 import Body from './components/Body';
 import Half from './components/Half';
 import End from './components/End';
-import Footer from './components/footer';
+import Last from './components/Last';
 import Screen from './components/Screen';
-
-
 
 const App = () => {
   return (
     <>
-    <Header />  
+    <Begin />  
     < Hero/>
     <Home/>
     <Body/>
     <Half/>
     <End/>
-    <Footer/>
+    <Last />
     <Screen/>
     </>
   )
