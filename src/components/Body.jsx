a6dc1ad99a35f32337c11eb8e9f0ca3bab7a03ac
react-project <ikemjoy57@gmail.com>
2025-04-14 09:26:06 +0100

    new changes made

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -6,23 +6,21 @@ import ikem from '../assets/images/ikem.png';
 import ruth from '../assets/images/ruth.png';
 import maydive from '../assets/images/maydive.png';
 
-const Section = ({ number, title, description, image, reverse }) => {
+const Section = ({ number, title, description, image }) => {
   return (
-    <div className={`flex ${reverse ? 'flex-row-reverse' : 'flex-row'} items-center justify-between w-full py-10 px-5 sm:px-16`}>
-      <div className="w-full sm:w-1/2 h-[60vh]">
-        <img src={image} alt={title} className="w-[100vw] h-full object-cover " />
-      </div>
-      <div className="w-full sm:w-1/2 bg-slate-100 p-6 flex flex-col justify-center relative h-[60vh]">
-        <div className="relative">
-          <p className="text-8xl sm:text-9xl text-gray-500 font-bold">{number}</p>
-          <p className="absolute text-lg sm:text-3xl font-bold text-gray-900" style={{ top: '60px' }}>
-            {title}
+    <div className="border shadow-md p-4 sm:p-6 mb-8 mx-4 sm:mx-16">
+      <div className="flex flex-col sm:flex-row">
+        <div className="w-full sm:w-1/2 mb-4 sm:mb-0">
+          <img src={image} className="w-full h-full object-cover" />
+        </div>
+        <div className="w-full sm:w-1/2 sm:pl-6 flex flex-col justify-center">
+          <p className="relative text-6xl sm:text-8xl text-gray-500 font-bold">{number}</p>
+          <p className="mt-[-40px] text-lg sm:text-2xl font-bold text-gray-900">{title}</p>
+          <p className="mt-4 sm:mt-6 text-base sm:text-xl">{description}</p>
+          <p className="text-green-500 hover:text-red-500 transition-colors duration-300 text-base sm:text-lg font-mono mt-3 cursor-pointer">
+            Learn More
           </p>
         </div>
-        <p className="mt-8 sm:mt-10 text-xl sm:text-2xl">{description}</p>
-        <p className="text-green-500 hover:text-red-500 transition-colors duration-300 text-lg sm:text-xl font-mono mt-3">
-          Learn More
-        </p>
       </div>
     </div>
   );
@@ -30,47 +28,43 @@ const Section = ({ number, title, description, image, reverse }) => {
 
 const Body = () => {
   return (
-    <section className="w-full">
+    <section className="py-10">
       <Section
+        image={pete}
         number="01"
         title="Product Development"
         description="We have over a decade of experience in Product Development."
-        image={pete}
       />
       <Section
+        image={bla}
         number="02"
         title="UX Research"
         description="Our Product Design Unit focuses on in-depth UX research."
-        image={bla}
-        reverse
       />
       <Section
+        image={ikem}
         number="03"
         title="I.T Consultancy"
         description="Leverage our experience in both business development and IT."
-        image={ikem}
       />
       <Section
+        image={ruth}
         number="04"
         title="Quality Assurance"
         description="Rethink your company's offering with a digital-first approach."
-        image={ruth}
-        reverse
       />
       <Section
+        image={daven}
         number="05"
         title="Mobile App Development"
         description="Create mobile apps with excellent usability."
-        image={daven}
       />
       <Section
+        image={maydive}
         number="06"
         title="Talent Outsourcing"
         description="Get well-trained and experienced Developers."
-        image={maydive}
-        reverse
       />
-      <div className="bg-white h-[15vh] w-full"></div>
     </section>
   );
 };
